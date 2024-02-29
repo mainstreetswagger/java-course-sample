0bada496d8b3d0ca05941bda90e4fe779179687e
Kanat <mainstreetswagger@gmail.com>
2024-02-29 16:37:59 +0100

    java

--- Program.java
+++ Program.java
@@ -2,7 +2,8 @@ import java.util.*;
 import java.util.function.BiConsumer;
 
 public class Program {
+    //hello java!
     public static void main(String[] args) {
-        System.out.println("Hello, Java!");
+        System.out.println("Hello, Java!!!!");
     }
 }
